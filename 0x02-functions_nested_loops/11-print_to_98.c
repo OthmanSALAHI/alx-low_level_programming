b8a3c9b1630798d01ae46780d3622c353bd71db3
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-20 17:09:25 +0100

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * print_to_98 - it prints till 98
+ * @n: Check the input
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n == 98)
+	{
+		printf("98");
+	} else if (n >= 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d, ", i);
+		}
+	} else if (n <= 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d ,", i);
+		}
+	}
+}
