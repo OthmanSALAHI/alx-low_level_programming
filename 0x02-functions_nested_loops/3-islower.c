cad24c7b77673e2aaadb53b04d862af4cca4770f
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-20 11:26:19 +0100

    c is fun

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ *
+ * _islower - function that checks for lowercase character
+ *
+ * Return : if lower Always 1.
+ *
+ * Return : if upper Always 0.
+ */
+
+int _islower(int c)
+{
+		if (c >= 97 && c <= 122)
+			return (1);
+		return (0);
+}
