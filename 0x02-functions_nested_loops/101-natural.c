ca09cd44e987a6bf33eca427d5575f416f61a1b1
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-20 20:13:41 +0100

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,17 +6,15 @@
  */
 int main(void)
 {
-	int i;
-	long int sum;
+	int i, sum = 0;
 
-	sum = 0;
-	for (i = 0; i <= 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			sum = sum + i;
 		}
 	}
-	printf("%lu\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
