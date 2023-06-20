cbd2e8995ec790759bcd26de4d15f81237f70a3e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-20 12:55:49 +0100

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,17 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int a;
 
-	return (ld);
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
