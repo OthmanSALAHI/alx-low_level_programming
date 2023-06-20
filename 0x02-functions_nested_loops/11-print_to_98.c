0087d67dea2d2a9a8d2935366d228d82d80c5ac2
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-20 17:10:37 +0100

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,13 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 		printf("98");
-	} else if (n >= 98)
+	} else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d, ", i);
 		}
-	} else if (n <= 98)
+	} else if (n < 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
