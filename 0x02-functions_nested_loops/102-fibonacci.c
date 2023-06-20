4c4bd71225e91a9165481436c5db34ad219a3de5
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-20 21:05:45 +0100

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 
 	fib[0] = 1;
 	fib[1] = 2;
-	printf("%lu, %lu, ",fib[0],fib[1]);
+	printf("%lu, %lu, ", fib[0], fib[1]);
 	for (i = 2; i < 50; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
