3261a8859286b14254a0cc343d3dd67639d4b6ea
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-22 19:43:01 +0100

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,17 +5,12 @@
  */
 void print_line(int n)
 {
-	int i;
+	char line = '_';
 
-	if (n <= 0)
+	while (n > 0)
 	{
-		_putchar('\n');
-	} else
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar(line);
+		n--;
 	}
+	_putchar('\n');
 }
