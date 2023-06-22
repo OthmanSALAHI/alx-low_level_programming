032e4e2c5b31add241fd12d7c21471ae55ac8e80
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-06-22 14:46:20 -0700

    100

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,4 +5,35 @@
  */
 void print_triangle(int size)
 {
+	char c     = '#';
+	char space = ' ';
+	int space_count = (size - 1);
+	int char_count  = (1);
+	int tmp;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (space_count >= 0)
+	{
+		tmp = space_count;
+		while (tmp > 0)
+		{
+			_putchar(space);
+			tmp--;
+		}
+		tmp = char_count;
+		while (tmp > 0)
+		{
+			_putchar(c);
+			tmp--;
+		}
+
+		_putchar('\n');
+		char_count++;
+		space_count--;
+	}
+}
