ca988adb48e36e118f66a0e571d5eb2ac10e883d
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-07-05 20:20:56 +0100

    powered

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,8 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int check(int a, int b);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
