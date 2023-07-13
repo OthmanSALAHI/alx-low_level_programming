0f9b855b9db054182d8277fdef86f00ba705f54b
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-07-13 17:48:00 +0100

    add the string head

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
