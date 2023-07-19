1461e5158d613dad2ffd28bef97236e3bdf52de2
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-07-19 11:23:11 -0700

    commiting

---
+++ 0x0F-function-pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: the name
+ * @f: function of name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		return (name);
+}
---
+++ 0x0F-function-pointers/function_pointers.h
@@ -0,0 +1,3 @@
+#ifdef FUNCTION_POINTERS_H_
+#include <stdio.h>
+void print_name(char *name, void (*f)(char *));
