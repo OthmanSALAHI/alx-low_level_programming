ea2884f0d5df499d5b1344f9c9ef8bdbcecd9094
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-14 12:10:45 +0100

    first linked list int

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+/* includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* structures */
+
+typedef struct dlistint_s {
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* functions */
+
+size_t print_dlistint(const dlistint_t *h);
+#endif
