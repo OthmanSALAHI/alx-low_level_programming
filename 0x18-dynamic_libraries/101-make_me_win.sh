02b26eb500ec4023655ff64e889b69895001024e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-19 22:50:44 +0100

    please

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Othmansalahi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+cp ../sh2.sh .
+. ./sh2.sh
