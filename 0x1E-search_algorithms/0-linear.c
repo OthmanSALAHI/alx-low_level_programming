689be2fcb8deaa1344aa888ceae3f9182dfba377
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-15 13:07:41 +0100

    linear algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	if (!array || !size)
 		return (-1);
 	i = 0;
-	while (i <= (int)size)
+	while (i < (int)size)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 		if (value == array[i])
