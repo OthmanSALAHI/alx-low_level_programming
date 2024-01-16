83153d06b556a7674a114afcb3069040e8ddb9a2
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-16 10:10:50 +0100

    push header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+int _recursive(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
